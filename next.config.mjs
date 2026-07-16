5a9c595fbb65c4b6fcddbc85cffaec4b11c28c17
didiola <onaolapooluwaseun98@gmail.com>
2026-07-16 12:27:22 +0100

    Add redirects for conference short links

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/conference",
+        destination: "https://conference.futurepathways.ng",
+        permanent: true,
+      },
+      {
+        source: "/delegate",
+        destination: "https://conference.futurepathways.ng/delegate",
+        permanent: true,
+      },
+      {
+        source: "/volunteer",
+        destination: "https://conference.futurepathways.ng/volunteer",
+        permanent: true,
+      },
+      {
+        source: "/speakers",
+        destination: "https://conference.futurepathways.ng/speakers",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
